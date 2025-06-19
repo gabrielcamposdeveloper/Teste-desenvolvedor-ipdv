8d05da55fccbb91c5d124e99ed819800a4723fb3
Gabriel Campos <gabrielcampos.dev@outlook.com>
2025-06-19 12:45:07 -0300

    [FIX] Correção function criar_usuario_com_credenciais

--- data/3_CreateFunctions.sql
+++ data/3_CreateFunctions.sql
@@ -8,8 +8,10 @@ CREATE OR REPLACE FUNCTION criar_usuario_com_credenciais(
     p_cargo_id INTEGER,
     p_senha TEXT
 ) RETURNS VOID AS $$
+DECLARE
+    usuario_id INTEGER;
 BEGIN
-    -- Verifica se email ou cpf já existem
+    -- Verifica se email ou cpf existem
     IF EXISTS (SELECT 1 FROM usuarios WHERE email = p_email OR cpf = p_cpf) THEN
         RAISE EXCEPTION 'Email ou CPF já cadastrado.';
     END IF;
@@ -17,9 +19,10 @@ BEGIN
     -- Insere usuário
     INSERT INTO usuarios (nome, email, cpf, telefone, cargo_id)
     VALUES (p_nome, p_email, p_cpf, p_telefone, p_cargo_id)
-    RETURNING id INTO STRICT usuario_id;
+    RETURNING id INTO usuario_id;
 
     -- Insere credenciais
-    INSERT INTO credenciais (usuario_id, senha) VALUES (usuario_id, p_senha);
+    INSERT INTO credenciais (usuario_id, senha) 
+    VALUES (usuario_id, p_senha);
 END;
 $$ LANGUAGE plpgsql;
