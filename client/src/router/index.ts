dcb171e835e7368897f1c7d0eb3c1a7259e85dab
Gabriel Campos <gabrielcampos.dev@outlook.com>
2025-06-23 20:51:53 -0300

    [Update] Vue auth

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -3,16 +3,41 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
+    {
+      path: '/',
+      name: 'login',
+      component: () => import('../views/auth/login.vue')
+    },
+{
+  path: '/dashboard',
+  name: 'dashboard',
+  component: () => import('@/views/dashboard.vue'),
+  meta: { requiresAuth: true }
+},
+{
+  path: '/usuario/cadastrar',
+  name: 'CadastroUsuario',
+  component: () => import('@/views/user/cadastroUsuario.vue'),
+  meta: { requiresAuth: true }
+},
+{
+  path: '/cargo/cadastrar',
+  name: 'CadastroCargo',
+  component: () => import('@/views/cargo/cadastroCargo.vue'),
+  meta: { requiresAuth: true }
+}
 
-    // },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+
+  if (to.meta.requiresAuth && !token) {
+    next({ name: 'login' })
+  } else {
+    next()
+  }
 })
 
 export default router
